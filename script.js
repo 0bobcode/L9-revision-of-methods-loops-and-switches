8cf83a4d59bb51a9869068bdf65d76592ce24c4e
bobchopra <bobchopracoding@gmail.com>
2024-09-06 09:46:54 -0400

    add for loop and its syntax

---
+++ script.js
@@ -0,0 +1,7 @@
+//loops
+for(let i = 1; i<=5; i++){
+    console.log(`horray we are picking apples ${i}`)
+}
+//let i = 1:starts with number 1
+//i<=5:this is a condition that keeps going as long as i is less than or equal to 5
+//i++: it is an incriment oporater wich after each loop increases i by 1
